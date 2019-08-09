f9b2061667457f929d2ca22f5f977536f4828bc7
Christopher Diggins <cdiggins@gmail.com>
2019-08-08 22:15:04 -0400

    Version 1.0 of BFast

---
+++ csharp/src/Buffers.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace Vim
+{
+    /// <summary>
+    /// Wraps an object that provides a span of bytes, for example a Memory object.
+    /// This is useful because we cannot have a collection of Span[byte], but 
+    /// we can have a collection of IBuffer. 
+    /// </summary>
+    public interface IBuffer
+    {
+        Span<byte> Bytes { get; }
+    }
+
+    /// <summary>
+    /// Represents a buffer associated with a string name. 
+    /// </summary>
+    public interface INamedBuffer : IBuffer
+    {
+        string Name { get; }
+    }
+
+    /// <summary>
+    /// A memory buffer is a concrete implementation of IBuffer
+    /// </summary>
+    public class MemoryBuffer<T> : IBuffer where T : struct
+    {
+        public MemoryBuffer(Memory<T> memory) => Memory = memory;
+        public Memory<T> Memory { get; }
+        public Span<byte> Bytes => MemoryMarshal.Cast<T, byte>(Memory.Span);
+    }
+
+    /// <summary>
+    /// Associates a buffer implementation with a name.
+    /// </summary>
+    public class NamedBuffer : INamedBuffer
+    {
+        public NamedBuffer(IBuffer buffer, string name = "")
+        {
+            Buffer = buffer;
+            Name = name;
+        }
+
+        public IBuffer Buffer { get; }
+        public Span<byte> Bytes => Buffer.Bytes;
+        public string Name { get; }
+    }
+
+    /// <summary>
+    /// Helper functions for working with buffers 
+    /// </summary>
+    public static class BufferExtensions
+    {
+        public static IBuffer ToBuffer<T>(this Span<T> span) where T : struct
+            => span.ToArray().ToBuffer();
+
+        public static IBuffer ToBuffer<T>(this Memory<T> memory) where T : struct
+            => new MemoryBuffer<T>(memory);
+
+        public static IBuffer ToBuffer<T>(this T[] xs) where T : struct
+            => new Memory<T>(xs).ToBuffer();
+
+        public static IBuffer ToBuffer(this string self)
+            => System.Text.Encoding.UTF8.GetBytes(self).ToBuffer();
+
+        public static IBuffer ToBuffer(this IEnumerable<string> strings)
+            => string.Join("\0", strings).ToBuffer();
+
+        public static INamedBuffer ToNamedBuffer<T>(this T[] xs, string name = "") where T: struct
+            => xs.ToBuffer().ToNamedBuffer(name);
+
+        public static INamedBuffer ToNamedBuffer(this IBuffer buffer, string name = "")
+            => new NamedBuffer(buffer, name);
+
+        public static INamedBuffer ToNamedBuffer<T>(this Span<T> span, string name = "") where T : struct
+            => span.ToBuffer().ToNamedBuffer(name);
+
+        public static INamedBuffer ToNamedBuffer<T>(this Memory<T> memory, string name = "") where T : struct
+            => memory.ToBuffer().ToNamedBuffer(name);
+
+        public static INamedBuffer ToNamedBuffer(this string self, string name = "")
+            => self.ToBuffer().ToNamedBuffer(name);
+
+        public static IBuffer ToNamedBuffer(this IEnumerable<string> strings, string name = "")
+            => strings.ToBuffer().ToNamedBuffer(name);
+
+        public static IEnumerable<INamedBuffer> ToNamedBuffers(this IEnumerable<IBuffer> buffers, IEnumerable<string> names = null)
+            => names == null ? buffers.Select(b => b.ToNamedBuffer("")) : buffers.Zip(names, ToNamedBuffer);
+
+        public static IDictionary<string, INamedBuffer> ToDictionary(this IEnumerable<INamedBuffer> buffers)
+            => buffers.ToDictionary(b => b.Name, b => b);
+
+        public static Memory<byte> ToMemory(this IBuffer buffer)
+            => buffer is MemoryBuffer<byte> mb ? mb.Memory : new Memory<byte>(buffer.Bytes.ToArray());
+
+        public static IEnumerable<INamedBuffer> ToNamedBuffers(this IDictionary<string, IBuffer> d)
+            => d.Select(kv => kv.Value.ToNamedBuffer(kv.Key));
+
+        public static IEnumerable<INamedBuffer> ToNamedBuffers(this IDictionary<string, byte[]> d)
+            => d.Select(kv => kv.Value.ToNamedBuffer(kv.Key));
+
+        public static Span<T> AsSpan<T>(this IBuffer buffer) where T : struct
+            => MemoryMarshal.Cast<byte, T>(buffer.Bytes);
+
+        public static string GetString(this IBuffer buffer)
+            => System.Text.Encoding.UTF8.GetString(buffer.Bytes.ToArray());
+
+        public static string[] GetStrings(this IBuffer buffer)
+            => buffer.GetString().Split('\0');
+
+        public static byte[] ToBytes<T>(this T[] xs) where T : struct
+            => MemoryMarshal.Cast<T, byte>(xs).ToArray();
+
+        public static byte[] ToBytes<T>(this T x) where T : struct
+            => ToBytes(new[] { x });
+    }
+}
---
+++ readme.md
@@ -0,0 +1,61 @@
+# BFAST
+
+BFAST stands for the **B**inary **F**ormat for **A**rray **S**erialization and **T**ransmission. 
+
+BFAST is a data format for simple, efficient, and reliable serialization and deserialization of 
+collections of binary data with optional names as a single block of data. 
+
+## Use Case
+
+You would use the BFAST structure if you have a binary data to serialize that is mostly in the form of 
+long arrays. For example a set of files that you want to bundle together without wanting to bring in 
+the overhead of a compression library or re-implementing TAR.
+We use BFAST to encode mesh data and as an archive format.
+
+## Features
+
+* Very small implementation overhead 
+* Easy to implement efficient and conformant encoders and decoders in different languages 
+* Format and endianess easily identified through a magic number at the front of the file
+* Data arrays are 32 byte aligned - facilitates casting to SIMD data types (eg. SSE2 types)
+* Array offsets are encoded using 64-bits to supports large data sets
+* Positions of data buffers are encoded in the beginning of the file
+
+# Specification
+
+The file format consists of three sections:
+
+* Header - Fixed size descriptor (32 byte) describing the file contents   
+* Ranges - An array of offset pairs indicating where each data buffers starts and finishes
+* Data   - 32-byte aligned concatenated data buffers. Each data buffer is 32-byte aligned. 
+
+The header has the following layout:  
+
+```
+    [StructLayout(LayoutKind.Explicit, Pack = 4, Size = 32)]
+    public struct Header
+    {
+        [FieldOffset(0)]    public long Magic;         // 0xBFA5
+        [FieldOffset(8)]    public long DataStart;     // <= File size and >= 32 + Sizeof(Range) * NumArrays 
+        [FieldOffset(16)]   public long DataEnd;       // >= DataStart and <= file size
+        [FieldOffset(24)]   public long NumArrays;     // Number of arrays
+    }
+```
+
+The ranges start at byte 32. There are `NumArrays` of them and they have has the following format. 
+The `Begin` and `End` values are byte offsets relative to the beginning of the file:
+
+```
+    [StructLayout(LayoutKind.Explicit, Pack = 4, Size = 16)]
+    public struct Range
+    {
+        [FieldOffset(0)] public long Begin;
+        [FieldOffset(8)] public long End;
+    }		
+```
+
+The data section (the `DataStart` field of the Header, starts at the first 32 byte 
+aligned address immediately following the last `Range` value.
+
+The first data buffer contain the names of the subsequent buffers as a concatenated list of Utf-8 encoded 
+strings separated by null characters.
